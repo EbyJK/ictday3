862f6848f9fe19a5c70856911d44181620208fae
Eby Joshy Kavungal <ebyjkavungal@gmail.com>
2024-07-13 16:56:37 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,18 @@ import ApiGet from './components/ApiGet'
 import CardApi from './components/CardApi'
 import CardApi2 from './components/CardApi2'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      {/* <TextInput /> */}
+       {/* <TextInput /> */}
       <br />
-      {/* <ApiGet/> */}
+       {/* <ApiGet/>  */}
       {/* <CardApi/> */}
-      <CardApi2/>
+       <CardApi2/> 
+    
     </>
   )
 }
